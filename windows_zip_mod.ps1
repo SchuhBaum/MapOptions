4a798511f82bcde75206e3f4a6c9351465d819ea
schuhbaum <schuhbaum71@gmail.com>
2025-10-04 10:20:56 +0200

    -

--- windows_zip_mod.ps1
+++ windows_zip_mod.ps1
@@ -21,6 +21,13 @@ $zip_path = "$downloads_path\$mod_name.zip"
 
 Push-Location $dir
 
+$item_paths = Get-ChildItem $mod_name -Filter "*region_names*" -File -Recurse | Select-Object -ExpandProperty FullName;
+foreach ($item_path in $item_paths) {
+    if (Test-Path $item_path) {
+        Remove-Item -Path $item_path -Force
+    }
+}
+
 7z a $zip_path "$mod_name\plugins\$dll_name" "$mod_name\plugins\$pdb_name" "$mod_name\modinfo.json" "$mod_name\thumbnail.png" "$mod_name\workshopdata.json"
 
 Pop-Location
